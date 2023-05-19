b4a14ca19db21752dfab4799a854f9a6368ce9d4
mntzd <julien.leske@gmail.com>
2023-05-19 09:06:48 +0200

    Musterlösungen to Challenges added

---
+++ state2/react-state-2_colored-number/src/components/Counter.js
@@ -0,0 +1,71 @@
+import ColoredNumber from "./ColoredNumber";
+import { useState } from "react";
+
+export default function Counter() {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div className="counter">
+      <h1>Colored Number</h1>
+      <ColoredNumber value={value} />
+      <div className="counter__buttons-container">
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="increment count"
+          onClick={() => {
+            setValue(value + 1);
+          }}
+        >
+          +
+        </button>
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="decrement count"
+          onClick={() => {
+            setValue(value - 1);
+          }}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
+
+// import ColoredNumber from "./ColoredNumber";
+// import { useState } from "react";
+
+// export default function Counter() {
+//   const [counter, setCounter] = useState(0);
+
+//   return (
+//     <div className="counter">
+//       <h1>Colored Number</h1>
+//       <ColoredNumber value={0} />
+//       <div className="counter__buttons-container">
+//         <button
+//           type="button"
+//           className="counter__button"
+//           aria-label="increment count"
+//           onClick={() => {
+//             setCounter(counter + 1);
+//           }}
+//         >
+//           +
+//         </button>
+//         <button
+//           type="button"
+//           className="counter__button"
+//           aria-label="decrement count"
+//           onClick={() => {
+//             setCounter(counter - 1);
+//           }}
+//         >
+//           -
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
---
+++ state2/react-state-2_table-reservation/src/MusterlösungApp.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import "./styles.css";
+
+export default function App() {
+  const [people, setPeople] = useState(0);
+  return (
+    <div className="container">
+      <h1>Place a Table Reservation</h1>
+      <Counter
+        onIncrement={() => setPeople(people + 1)}
+        onDecrement={() => setPeople(people - 1)}
+      />
+      <p>You are going to reserve a table for {people} people.</p>
+    </div>
+  );
+}
